b97664ea08f0f166428145e03994d783ae8eeb70
Karol Moroz <karol@moroz.dev>
2023-08-16 23:24:22 +0200

    Play around with DbContext

--- DbContext.cs
+++ DbContext.cs
@@ -1,31 +1,54 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using EFCore.NamingConventions;
+using System.Runtime.InteropServices;
 
 namespace ConsoleApp.PostgreSQL
 {
   public class BloggingContext : DbContext
   {
-    public DbSet<Blog> Blogs { get; set; }
-    public DbSet<Post> Posts { get; set; }
+    public DbSet<Blog>? Blogs { get; set; }
+    public DbSet<Post>? Posts { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    => optionsBuilder.UseNpgsql("Host=localhost;Database=efcore_demo;Username=postgres;Password=postgres").UseSnakeCaseNamingConvention();
+    => optionsBuilder.UseNpgsql(GetDatabaseConnectionString()).UseSnakeCaseNamingConvention();
+
+    private string GetDatabaseConnectionString()
+    {
+      var connStr = Environment.GetEnvironmentVariable("DATABASE_URL");
+      if (string.IsNullOrWhiteSpace(connStr))
+      {
+        throw new ArgumentNullException("Environment variable DATABASE_URL is not set!");
+      }
+
+      var parsed = new Uri(connStr);
+      if (parsed == null)
+      {
+        throw new Exception("Could not parse DATABASE_URL");
+      }
+
+      var userInfo = parsed.UserInfo.Split(':', 2);
+      var username = userInfo[0];
+      var password = userInfo[1];
+      var passwordSection = password == null ? "" : $";Password={password}";
+
+      return $"Host={parsed.Host};Database={parsed.AbsolutePath.TrimStart('/')};Username={username}{passwordSection}";
+    }
   }
 
   public class Blog
   {
     public int Id { get; set; }
-    public string Url { get; set; }
-    public List<Post> Posts { get; set; }
+    public required string Url { get; set; }
+    public required List<Post> Posts { get; set; }
   }
 
   public class Post
   {
     public int Id { get; set; }
-    public string Title { get; set; }
-    public string Content { get; set; }
+    public required string Title { get; set; }
+    public required string Content { get; set; }
     public int BlogId { get; set; }
-    public Blog Blog { get; set; }
+    public Blog? Blog { get; set; }
   }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp.PostgreSQL;
+
+using var db = new BloggingContext();
+
+Console.WriteLine("Querying for posts");
+var posts = db.Posts!.OrderBy(b => b.Id)
+    .ToList();
+
+foreach (var post in posts)
+{
+  Console.WriteLine($"Post: {post.Title}");
+}
